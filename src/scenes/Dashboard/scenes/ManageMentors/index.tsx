bd756e91e5dbab6e4bdeb773b8b654593657a33e
anjula-sack <anjulashanaka@gmail.com>
2020-10-25 12:35:10 +0530

    Bind the get mentors API with the manager mentors view

--- src/scenes/Dashboard/scenes/ManageMentors/index.tsx
+++ src/scenes/Dashboard/scenes/ManageMentors/index.tsx
@@ -1,55 +1,76 @@
-import React from 'react';
-import { Typography, List, Button, Avatar } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Typography, List, Button, Avatar, notification, Spin } from 'antd';
 import { Mentor } from './interfaces';
+import { useParams } from 'react-router';
+import axios, { AxiosResponse } from 'axios';
 
 const { Title } = Typography;
 
-const listData: Mentor[] = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    id: i,
-    name: 'John Doe',
-    linkedinUrl: 'https://www.linkedin.com/',
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description: 'Brief Intro about the mentee.',
-  });
-}
-
 function ManageMentors() {
+  const { programId } = useParams();
+  const [mentors, setMentors] = useState<Mentor[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(`http://localhost:8080/api/scholarx/programs/${programId}/mentors`)
+      .then((result: AxiosResponse<Mentor[]>) => {
+        if (result.status == 200) {
+          setIsLoading(false);
+          setMentors(result.data);
+        } else {
+          throw new Error();
+        }
+      })
+      .catch(() => {
+        setIsLoading(false);
+        notification.warning({
+          message: 'Warning!',
+          description: 'Something went wrong when fetching the mentors',
+        });
+      });
+  }, []);
+
   return (
     <div>
       <Title>Manage Mentors</Title>
-      <List
-        itemLayout="horizontal"
-        size="large"
-        pagination={{
-          onChange: (page) => {
-            console.log(page);
-          },
-          pageSize: 8,
-        }}
-        dataSource={listData}
-        renderItem={(item) => (
-          <List.Item
-            actions={[
-              <Button key="edit" type="primary">
-                Edit
-              </Button>,
-              <Button key="more" type="default">
-                More
-              </Button>,
-            ]}
-          >
-            <List.Item.Meta
-              avatar={
-                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-              }
-              title={<a href="#">{item.name}</a>}
-              description={item.description}
-            />
-          </List.Item>
-        )}
-      />
+      <Spin tip="Loading..." spinning={isLoading}>
+        <List
+          itemLayout="horizontal"
+          size="large"
+          pagination={{
+            onChange: (page) => {
+              console.log(page);
+            },
+            pageSize: 8,
+          }}
+          dataSource={mentors}
+          renderItem={(item: Mentor) => (
+            <List.Item
+              key={item.id}
+              actions={[
+                <Button key="edit" type="primary">
+                  Edit
+                </Button>,
+                <Button key="more" type="default">
+                  More
+                </Button>,
+              ]}
+            >
+              <List.Item.Meta
+                avatar={<Avatar src={item.profile.imageUrl} />}
+                title={
+                  <a href="#">
+                    {item.profile.firstName} {item.profile.lastName}
+                  </a>
+                }
+                description={item.profile.headline}
+              />
+            </List.Item>
+          )}
+        />
+      </Spin>
     </div>
   );
 }
