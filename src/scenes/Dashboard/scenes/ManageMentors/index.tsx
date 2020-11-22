2682116e66b16aed3293fd991d0acb83d7991a87
Gravewalker666 <heshanandrews99@gmail.com>
2020-11-22 23:17:05 +0530

    Implement manage mentor interface

--- src/scenes/Dashboard/scenes/ManageMentors/index.tsx
+++ src/scenes/Dashboard/scenes/ManageMentors/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Typography, List, Avatar, notification, Spin, Empty, Tag } from 'antd';
-import { Mentor } from './interfaces';
+import { Typography, List, notification, Spin, Empty, Col, Row } from 'antd';
+import { Mentor } from '../../../../interfaces';
 import { useParams } from 'react-router';
 import axios, { AxiosResponse } from 'axios';
-import MentorActions from './components/MentorActions';
-import RemoveMentor from './components/RemoveMentor';
+import styles from '../../styles.css';
+import { SavedProgram } from '../../../../interfaces';
+import MentorRow from './components/MentorRow';
 
 const { Title } = Typography;
 
@@ -12,137 +13,105 @@ function ManageMentors() {
   const { programId } = useParams();
   const [mentors, setMentors] = useState<Mentor[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [programState, setProgramState] = useState(String);
+  const [program, setProgram] = useState<SavedProgram | null>(null);
+  const [shouldLoadMentors, setShouldLoadMentors] = useState<boolean>(false);
 
   useEffect(() => {
     setIsLoading(true);
+    getProgram();
+  }, []);
+
+  const getProgram = () => {
     axios
-      .get(`http://localhost:8080/api/scholarx/programs/${programId}`)
-      .then((result: any) => {
+      .get(`http://localhost:8080/programs/${programId}`, {
+        withCredentials: true,
+      })
+      .then((result: AxiosResponse<SavedProgram>) => {
         if (result.status == 200) {
-          setProgramState(result.data.state);
+          setProgram(result.data);
+          const shouldLoadMentors = !(
+            result.data.state == 'CREATED' ||
+            result.data.state == 'COMPLETED' ||
+            result.data.state == 'REMOVED'
+          );
+          setShouldLoadMentors(shouldLoadMentors);
+          if (shouldLoadMentors) {
+            getMentors();
+          } else {
+            setIsLoading(false);
+          }
         } else {
           throw new Error();
         }
       })
       .catch(() => {
         setIsLoading(false);
-        notification.warning({
-          message: 'Warning!',
-          description: 'Something went wrong when fetching the programme detail',
+        notification.error({
+          message: 'Error!',
+          description: 'Something went wrong when fetching program details',
         });
       });
+  };
 
+  const getMentors = () => {
     axios
-      .get(`http://localhost:8080/api/scholarx/programs/${programId}/mentors`)
+      .get(`http://localhost:8080/programs/${programId}/mentors`, {
+        withCredentials: true,
+      })
       .then((result: AxiosResponse<Mentor[]>) => {
-        if (result.status == 200) {
-          setIsLoading(false);
+        if (result.status == 200 || result.status == 204) {
           setMentors(result.data);
+          setIsLoading(false);
         } else {
           throw new Error();
         }
       })
       .catch(() => {
         setIsLoading(false);
-        notification.warning({
-          message: 'Warning!',
+        notification.error({
+          message: 'Error!',
           description: 'Something went wrong when fetching the mentors',
         });
       });
-
-  }, []);
-
-  let dynamicComponent;
-
-  switch (programState) {
-    case 'CREATED':
-    case 'COMPLETED':
-    case 'REMOVED':
-      dynamicComponent =
-        <Empty
-          image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
-          imageStyle={{
-            height: 60,
-          }}
-          description={
-            <span>
-              You are currently in {programState} state.
-            </span>
-          }
-        >
-        </Empty>
-      break;
-    case 'MENTOR_APPLICATION':
-    case 'MENTOR_SELECTION':
-    case 'MENTEE_APPLICATION':
-    case 'MENTEE_SELECTION':
-    case 'ONGOING':
-      dynamicComponent =
-        
-          <List
-            itemLayout="horizontal"
-            size="large"
-            pagination={{
-              onChange: (page) => {
-                console.log(page);
-              },
-              pageSize: 8,
-            }}
-            dataSource={mentors}
-            renderItem={(item: Mentor) => {
-              let tagComponent;
-              switch (item.state) {
-                case 'PENDING':
-                  tagComponent = <Tag color="blue">PENDING</Tag>
-                  break;
-                case 'APPROVED':
-                  tagComponent = <Tag color="green">APPROVED</Tag>
-                  break;
-                case 'REJECTED':
-                  tagComponent = <Tag color="orange">REJECTED</Tag>
-                  break;
-                case 'REMOVED':
-                  tagComponent = <Tag color="red">REMOVED</Tag>
-                  break;
-              }
-              let actionButton;
-              if (programState === 'MENTOR_SELECTION') {
-                actionButton = <MentorActions id={item.id} state={item.state} />;
-              }
-
-              return (
-                <List.Item
-                  key={item.id}
-                  actions={
-                    [actionButton,<RemoveMentor id={item.id} />]
-                  }
-                >
-                  <List.Item.Meta
-                    avatar={<Avatar src={item.profile.imageUrl} />}
-                    title={
-                      <div>
-                        <a href={item.profile.linkedinUrl}>
-                          {item.profile.firstName} {item.profile.lastName}
-                        </a>
-                        <br/>
-                        {tagComponent}
-                      </div>
-                    }
-                    description={item.profile.headline}
-                  />   
-                </List.Item>
-              )
-            }}
-          />
-      break;
-  }
+  };
 
   return (
-    <div>
-      <Title>Manage Mentors</Title>
+    <div className={styles.container}>
       <Spin tip="Loading..." spinning={isLoading}>
-        {dynamicComponent}
+        <Row>
+          <Col md={3} />
+          <Col md={15}>
+            <Title>Manage Mentors</Title>
+            {!shouldLoadMentors && program != null && (
+              <Empty
+                image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+                imageStyle={{
+                  height: 60,
+                }}
+                description={
+                  <span>You are currently in {program.state} state.</span>
+                }
+              />
+            )}
+            {shouldLoadMentors && (
+              <List
+                itemLayout="horizontal"
+                size="large"
+                pagination={{
+                  pageSize: 8,
+                }}
+                dataSource={mentors}
+                renderItem={(mentor: Mentor) => (
+                  <MentorRow
+                    key={mentor.id}
+                    mentor={mentor}
+                    programState={program.state}
+                  />
+                )}
+              />
+            )}
+          </Col>
+        </Row>
       </Spin>
     </div>
   );
