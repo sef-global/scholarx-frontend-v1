f5647c6bdba647a2bdd839651e9037d19871c5df
Heshan Andrews <45477334+Gravewalker666@users.noreply.github.com>
2021-04-21 10:33:50 +0530

    Add signout option for the users (#113)
    Add logout buttons to user navbar and admin navabar. Change auth_url and Add logout_url to the constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const API_URL = 'http://localhost:8080/api';
-export const AUTH_URL = 'http://localhost:8080//oauth2/authorization/linkedin';
+export const AUTH_URL = 'http://localhost:8080/login';
+export const LOGOUT_URL = 'http://localhost:8080/logout';
