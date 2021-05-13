9d9ec723bfa6e39c41098a0b68af0b4888ab2354
Nethmi Rodrigo <neth23rodrigo@gmail.com>
2021-05-13 09:44:33 +0530

    Apply as a mentor application to display dynamic questions

--- src/scenes/Home/scenes/EditMentorApplication/index.tsx
+++ src/scenes/Home/scenes/EditMentorApplication/index.tsx
@@ -1,4 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router';
+import axios, { AxiosResponse } from 'axios';
 import {
   Button,
   Row,
@@ -8,59 +10,49 @@ import {
   notification,
   Spin,
   Typography,
+  Card,
+  Steps,
 } from 'antd';
-import styles from './styles.css';
-import { useHistory, useParams } from 'react-router';
-import axios, { AxiosResponse } from 'axios';
-import {
-  Mentor,
-  Application,
-  SavedProgram,
-  Profile,
-} from '../../../../interfaces';
-import mainStyles from '../../styles.css';
-import { ArrowLeftOutlined } from '@ant-design/icons';
+
 import NavigationBar from '../../components/NavigationBar';
 import Footer from '../../components/Footer';
-import { API_URL } from '../../../../constants';
 import LogInModal from '../../../../components/LogInModal';
 import { UserContext } from '../../../../index';
+import Result from '../../components/ResultScreen';
+
+import {
+  SavedProgram,
+  Profile,
+  QuestionResponse,
+  UpdateQuestion,
+} from '../../../../interfaces';
+import { API_URL } from '../../../../constants';
+
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import { SaveFilled } from '@ant-design/icons';
+
+import styles from './styles.css';
+import mainStyles from '../../styles.css';
 
 const { TextArea } = Input;
 const { Title } = Typography;
+const { Step } = Steps;
 
 function EditMentorApplication() {
   const [form] = Form.useForm();
   const { programId } = useParams();
   const [programTitle, setProgramTitle] = useState<string>('');
+  const [responses, setResponses] = useState<QuestionResponse[]>([]);
+  const [isApplySuccess, setIsApplySuccess] = useState(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [current, setCurrent] = useState<QuestionResponse>();
+  const [values, setValues] = useState<QuestionResponse[]>([]);
+  const [step, setStep] = useState<number>(0);
   const user: Partial<Profile | null> = useContext(UserContext);
   const history = useHistory();
 
   useEffect(() => {
     setIsLoading(true);
-    axios
-      .get(`${API_URL}/programs/${programId}/mentor`, {
-        withCredentials: true,
-      })
-      .then((result: AxiosResponse<Mentor>) => {
-        if (result.status == 200) {
-          setIsLoading(false);
-          form.setFieldsValue({
-            application: result.data.application,
-            prerequisites: result.data.prerequisites,
-          });
-        } else {
-          throw new Error();
-        }
-      })
-      .catch(() => {
-        setIsLoading(false);
-        notification.warning({
-          message: 'Warning!',
-          description: 'Something went wrong when fetching the program',
-        });
-      });
     getProgram();
   }, []);
 
@@ -72,7 +64,7 @@ function EditMentorApplication() {
       .then((result: AxiosResponse<SavedProgram>) => {
         if (result.status == 200) {
           setProgramTitle(result.data.title);
-          setIsLoading(false);
+          getResponses();
         } else {
           throw new Error();
         }
@@ -86,34 +78,119 @@ function EditMentorApplication() {
       });
   };
 
-  const apply = (values: any) => {
+  const getResponses = () => {
     setIsLoading(true);
-    const application: Application = {
-      application: values.application,
-      prerequisites: values.prerequisites,
-    };
     axios
-      .put(`${API_URL}/programs/${programId}/application`, application, {
+      .get(`${API_URL}/programs/${programId}/responses/mentor`, {
         withCredentials: true,
       })
-      .then((result: AxiosResponse<Mentor>) => {
+      .then((result: AxiosResponse<QuestionResponse[]>) => {
         if (result.status == 200) {
-          setIsLoading(false);
-          notification.success({
-            message: 'Success!',
-            description: 'Successfully applied!',
-          });
+          setResponses(JSON.parse(JSON.stringify(result.data)));
+          setValues(JSON.parse(JSON.stringify(result.data)));
+          setCurrent(result.data[0]);
+          setStep(0);
         } else {
+          setIsLoading(false);
           throw new Error();
         }
+        setIsLoading(false);
       })
       .catch(() => {
         setIsLoading(false);
         notification.warning({
           message: 'Warning!',
-          description: 'Something went wrong when applying for the program',
+          description: 'Something went wrong when fetching the program',
         });
       });
+  };
+
+  const apply = () => {
+    setIsLoading(true);
+    const application: UpdateQuestion[] = [];
+    let index: number;
+    let error: any;
+    values.forEach((response: QuestionResponse) => {
+      const prevRes: QuestionResponse = responses.find(
+        (element: QuestionResponse, i: number) => {
+          if (element.id.questionId === response.id.questionId) {
+            index = i;
+            return element;
+          }
+        }
+      );
+      if (values[index].response.length === 0) {
+        error = {
+          message: 'Values cannot be empty',
+        };
+        setCurrent(response);
+      } else if (values[index].response !== prevRes.response) {
+        const res: UpdateQuestion = {
+          id: {
+            questionId: prevRes.id.questionId,
+            mentorId: prevRes.id.mentorId,
+          },
+          response: values[index].response,
+        };
+        application.push(res);
+      }
+    });
+    if (application.length > 0) {
+      axios
+        .put(`${API_URL}/programs/${programId}/responses/mentor`, application, {
+          withCredentials: true,
+        })
+        .then((result: AxiosResponse<QuestionResponse[]>) => {
+          if (result.status == 200) {
+            notification.success({
+              message: 'Success!',
+              description: 'Successfully edited!',
+            });
+            setIsApplySuccess(true);
+          } else {
+            notification.warning({
+              message: 'Warning!',
+              description: 'Something went wrong when editing the program',
+            });
+            throw new Error();
+          }
+          setIsLoading(false);
+        })
+        .catch(() => {
+          setIsLoading(false);
+          notification.warning({
+            message: 'Warning!',
+            description: 'Something went wrong when editing the program',
+          });
+        });
+    } else {
+      notification.warning({
+        message: 'Warning!',
+        description: error ? error.message : 'Values have not been changed',
+      });
+      setIsLoading(false);
+    }
+  };
+
+  const onChange = (current: number) => {
+    setStep(current);
+    setCurrent(values[current]);
+  };
+
+  const onUpdateValues = (value: any) => {
+    const allvalues = [...values];
+    let index = 0;
+    const question: QuestionResponse = allvalues.find(
+      (element: QuestionResponse, i: number) => {
+        if (element.id.questionId === parseInt(Object.keys(value)[0], 10)) {
+          index = i;
+          return element;
+        }
+      }
+    );
+    question.response = Object.values(value)[0];
+    allvalues[index] = question;
+    setValues(allvalues);
   };
 
   return (
@@ -134,54 +211,108 @@ function EditMentorApplication() {
             />
           </Col>
         </Row>
-        <Row>
-          <Col md={2} />
-          <Col md={12}>
-            <Title level={1}>{programTitle}</Title>
-            <Title level={2}>Edit Mentor Application</Title>
-          </Col>
-        </Row>
         <Spin tip="Loading..." spinning={isLoading}>
-          <div className={styles.form}>
-            <Form layout="vertical" size="large" onFinish={apply} form={form}>
-              <Row>
-                <Col md={2} />
-                <Col md={12}>
-                  <Title level={4}>
-                    Why do you think you are suitable as a mentor in this
-                    program?
-                  </Title>
-                  <Form.Item name="application" rules={[{ required: true }]}>
-                    <TextArea rows={5} />
-                  </Form.Item>
-                </Col>
-              </Row>
-              <br />
-              <Row>
-                <Col md={2} />
-                <Col md={12}>
-                  <Title level={4}>
-                    Include the Pre requisites you expect from mentees
-                    <i>(This will be displayed in public)</i>
-                  </Title>
-                  <Form.Item name="prerequisites" rules={[{ required: true }]}>
-                    <TextArea rows={8} />
-                  </Form.Item>
-                </Col>
-              </Row>
-              <br />
-              <Row>
-                <Col md={2} />
-                <Col md={12}>
-                  <Button
-                    htmlType="submit"
-                    type="primary"
-                    className={styles.submitButton}
-                  >
-                    Submit
-                  </Button>
-                </Col>
-              </Row>
+          <div className={styles['form-container']}>
+            <Form
+              layout="vertical"
+              size="large"
+              onFinish={apply}
+              form={form}
+              onValuesChange={onUpdateValues}
+            >
+              {isApplySuccess ? (
+                <div className={styles.wrapper}>
+                  <Card className={styles.card}>
+                    <Result programId={programId} />
+                  </Card>
+                </div>
+              ) : (
+                <Card
+                  className={styles['form-card']}
+                  title={
+                    <>
+                      <Title level={2} className={styles['center-text']}>
+                        {programTitle}
+                      </Title>
+                      <Title
+                        level={3}
+                        className={styles['center-text']}
+                        type="secondary"
+                      >
+                        Mentor Application
+                      </Title>
+                    </>
+                  }
+                  actions={[
+                    <Button
+                      type="primary"
+                      key="previous-button"
+                      onClick={() => onChange(step - 1)}
+                      disabled={!(step > 0)}
+                    >
+                      Previous
+                    </Button>,
+                    <Button
+                      htmlType="submit"
+                      type="primary"
+                      className={styles.submitButton}
+                      icon={<SaveFilled />}
+                      key={'submit-button'}
+                      disabled={step !== values.length - 1}
+                    >
+                      Submit
+                    </Button>,
+                    <Button
+                      type="primary"
+                      key="next-button"
+                      onClick={() => onChange(step + 1)}
+                      disabled={!(step < values.length - 1)}
+                    >
+                      Next
+                    </Button>,
+                  ]}
+                >
+                  <Row>
+                    <Col span={6}>
+                      <Card title="Questions" style={{ textAlign: 'center' }}>
+                        <Steps
+                          direction="vertical"
+                          size="small"
+                          current={step}
+                          onChange={onChange}
+                        >
+                          {responses.map((response: QuestionResponse) => (
+                            <Step
+                              style={{ marginBottom: '0.5rem' }}
+                              key={response.id.questionId}
+                              title={`Question ${response.id.questionId}`}
+                              description={response.question.question}
+                            />
+                          ))}
+                        </Steps>
+                      </Card>
+                    </Col>
+                    <Col span={18}>
+                      {current && (
+                        <Card title={current.question.question}>
+                          <Form.Item
+                            initialValue={current.response}
+                            name={current.id.questionId}
+                            rules={[
+                              {
+                                required: true,
+                                message: 'This question cannot be left empty',
+                              },
+                            ]}
+                          >
+                            <TextArea rows={15} />
+                          </Form.Item>
+                        </Card>
+                      )}
+                    </Col>
+                  </Row>
+                </Card>
+              )}
             </Form>
           </div>
         </Spin>
--- src/scenes/Home/scenes/MentorApplication/index.tsx
+++ src/scenes/Home/scenes/MentorApplication/index.tsx
@@ -1,4 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router';
+import axios, { AxiosResponse } from 'axios';
 import {
   Button,
   Row,
@@ -9,27 +11,32 @@ import {
   Spin,
   Typography,
   Card,
+  Steps,
 } from 'antd';
-import styles from './styles.css';
-import { useHistory, useParams } from 'react-router';
-import axios, { AxiosResponse } from 'axios';
-import {
-  Mentor,
-  Application,
-  SavedProgram,
-  Profile,
-} from '../../../../interfaces';
-import mainStyles from '../../styles.css';
-import { ArrowLeftOutlined } from '@ant-design/icons';
+
 import NavigationBar from '../../components/NavigationBar';
 import Footer from '../../components/Footer';
-import { API_URL } from '../../../../constants';
 import { UserContext } from '../../../../index';
 import LogInModal from '../../../../components/LogInModal';
 import Result from '../../components/ResultScreen';
 
+import {
+  Application,
+  SavedProgram,
+  Profile,
+  MentorQuestion as Question,
+} from '../../../../interfaces';
+import { API_URL } from '../../../../constants';
+
+import { SaveFilled } from '@ant-design/icons';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+
+import styles from './styles.css';
+import mainStyles from '../../styles.css';
+
 const { TextArea } = Input;
 const { Title } = Typography;
+const { Step } = Steps;
 
 function MentorApplication() {
   const [form] = Form.useForm();
@@ -37,6 +44,10 @@ function MentorApplication() {
   const [programTitle, setProgramTitle] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isApplySuccess, setIsApplySuccess] = useState(false);
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [step, setStep] = useState<number>(0);
+  const [answers, setAnswers] = useState<any[]>([]);
+  const [currentQuestion, setcurrentQuestion] = useState<Question>();
   const user: Partial<Profile | null> = useContext(UserContext);
   const history = useHistory();
 
@@ -53,8 +64,9 @@ function MentorApplication() {
       .then((result: AxiosResponse<SavedProgram>) => {
         if (result.status == 200) {
           setProgramTitle(result.data.title);
-          setIsLoading(false);
+          getQuestions();
         } else {
+          setIsLoading(false);
           throw new Error();
         }
       })
@@ -67,31 +79,109 @@ function MentorApplication() {
       });
   };
 
-  const apply = (values: any) => {
-    setIsLoading(true);
-    const application: Application = {
-      application: values.application,
-      prerequisites: values.prerequisites,
-    };
+  const getQuestions = () => {
     axios
-      .post(`${API_URL}/programs/${programId}/mentor`, application, {
+      .get(`${API_URL}/programs/${programId}/questions/MENTOR`, {
         withCredentials: true,
       })
-      .then((result: AxiosResponse<Mentor>) => {
-        if (result.status == 201) {
-          setIsLoading(false);
-          setIsApplySuccess(true);
+      .then((result: AxiosResponse<Question[]>) => {
+        if (result.status === 200) {
+          setQuestions(result.data);
+          setcurrentQuestion(result.data[0]);
         } else {
-          throw new Error();
+          notification.error({
+            description: 'Something went wrong while fetching the errors',
+            message: 'Error!',
+          });
         }
+        setIsLoading(false);
       })
       .catch(() => {
-        setIsLoading(false);
-        notification.warning({
-          message: 'Warning!',
-          description: 'Something went wrong when applying for the program',
+        notification.error({
+          description: 'Something went wrong while fetching the errors',
+          message: 'Error!',
         });
+        setIsLoading(false);
       });
+  };
+
+  const apply = () => {
+    setIsLoading(true);
+    const application: Application[] = [];
+    let error: any = null;
+    questions.forEach((question: Question) => {
+      const answer = answers.find(
+        (element) => question.id === element.questionId
+      );
+      if (!answer) {
+        error = {
+          message: 'All questions must be answered',
+          questionId: question.id,
+        };
+        setcurrentQuestion(question);
+      } else {
+        const submitResponse: Application = {
+          question: {
+            id: question.id,
+          },
+          response: answer.response,
+        };
+        application.push(submitResponse);
+      }
+    });
+    if (error) {
+      notification.warning({
+        message: 'Warning!',
+        description: error.message,
+      });
+      setIsLoading(false);
+    } else {
+      axios
+        .post(`${API_URL}/programs/${programId}/mentor`, application, {
+          withCredentials: true,
+        })
+        .then((result: AxiosResponse<any>) => {
+          if (result.status == 201) {
+            setIsLoading(false);
+            setIsApplySuccess(true);
+            notification.success({
+              message: 'Success!',
+              description: 'Successfully applied!',
+            });
+          } else {
+            throw new Error();
+          }
+        })
+        .catch(() => {
+          setIsLoading(false);
+          notification.warning({
+            message: 'Warning!',
+            description: 'Something went wrong when applying for the program',
+          });
+        });
+    }
+  };
+
+  const updateValues = (value: any) => {
+    const questionId: number = parseInt(Object.keys(value)[0], 10);
+    const response = value[questionId];
+    const allanswers = [...answers];
+    let index = 0;
+    const prevAnswer = allanswers.find((answer, i) => {
+      index = i;
+      return answer.questionId === questionId;
+    });
+    if (prevAnswer) {
+      allanswers[index].response = response;
+    } else {
+      allanswers.push({ questionId, response });
+    }
+    setAnswers(allanswers);
+  };
+
+  const onChange = (currentStep: number) => {
+    setStep(currentStep);
+    setcurrentQuestion(questions[currentStep]);
   };
 
   return (
@@ -99,80 +189,119 @@ function MentorApplication() {
       <LogInModal isModalVisible={user === null} onCancel={null} />
       <NavigationBar />
       <div className={mainStyles.container}>
-        <Row>
-          <Col md={2} />
-          <Col md={12}>
-            <Button
-              shape="circle"
-              icon={<ArrowLeftOutlined />}
-              size="large"
-              onClick={() => {
-                history.goBack();
-              }}
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col md={2} />
-          <Col md={12}>
-            <Title level={1}>{programTitle}</Title>
-            <Title level={2}>Apply as Mentor</Title>
-          </Col>
-        </Row>
         <Spin tip="Loading..." spinning={isLoading}>
-          {isApplySuccess ? (
-            <div className={styles.wrapper}>
-              <Card className={styles.card}>
-                <Result programId={programId} />
-              </Card>
-            </div>
-          ) : (
-            <div className={styles.form}>
-              <Form layout="vertical" size="large" onFinish={apply} form={form}>
-                <Row>
-                  <Col md={2} />
-                  <Col md={12}>
-                    <Title level={4}>
-                      Why do you think you are suitable as a mentor in this
-                      program?
-                    </Title>
-                    <Form.Item name="application" rules={[{ required: true }]}>
-                      <TextArea rows={5} />
-                    </Form.Item>
-                  </Col>
-                </Row>
-                <br />
-                <Row>
-                  <Col md={2} />
-                  <Col md={12}>
-                    <Title level={4}>
-                      Include the Pre requisites you expect from mentees
-                      <i>(This will be displayed in public)</i>
-                    </Title>
-                    <Form.Item
-                      name="prerequisites"
-                      rules={[{ required: true }]}
+          <Row>
+            <Col md={2} />
+            <Col md={12}>
+              <Button
+                shape="circle"
+                icon={<ArrowLeftOutlined />}
+                size="large"
+                onClick={() => {
+                  history.goBack();
+                }}
+              />
+            </Col>
+          </Row>
+          <div className={styles['form-container']}>
+            <Form
+              layout="vertical"
+              size="large"
+              onFinish={apply}
+              form={form}
+              onValuesChange={updateValues}
+            >
+              {isApplySuccess ? (
+                <div className={styles.wrapper}>
+                  <Card className={styles.card}>
+                    <Result programId={programId} />
+                  </Card>
+                </div>
+              ) : (
+                <Card
+                  className={styles['form-card']}
+                  title={
+                    <>
+                      <Title level={2} className={styles['center-text']}>
+                        {programTitle}
+                      </Title>
+                      <Title
+                        level={3}
+                        className={styles['center-text']}
+                        type="secondary"
+                      >
+                        Mentor Application
+                      </Title>
+                    </>
+                  }
+                  actions={[
+                    <Button
+                      type="primary"
+                      onClick={() => onChange(step - 1)}
+                      disabled={!(step > 0)}
+                      key="prev-button"
                     >
-                      <TextArea rows={8} />
-                    </Form.Item>
-                  </Col>
-                </Row>
-                <br />
-                <Row>
-                  <Col md={2} />
-                  <Col md={12}>
+                      Previous
+                    </Button>,
                     <Button
                       htmlType="submit"
                       type="primary"
                       className={styles.submitButton}
+                      icon={<SaveFilled />}
+                      key={'submit-button'}
+                      disabled={step !== questions.length - 1}
                     >
                       Submit
-                    </Button>
-                  </Col>
-                </Row>
-              </Form>
-            </div>
-          )}
+                    </Button>,
+                    <Button
+                      type="primary"
+                      onClick={() => onChange(step + 1)}
+                      key="next-button"
+                      disabled={!(step < questions.length - 1)}
+                    >
+                      Next
+                    </Button>,
+                  ]}
+                >
+                  <Row>
+                    <Col span={6}>
+                      <Steps
+                        direction="vertical"
+                        size="small"
+                        current={step}
+                        onChange={onChange}
+                      >
+                        {questions.map((question: Question) => (
+                          <Step
+                            title={`Question ${question.id}`}
+                            description={question.question}
+                            key={question.id}
+                          />
+                        ))}
+                      </Steps>
+                    </Col>
+                    <Col span={18}>
+                      {currentQuestion && (
+                        <Card title={currentQuestion.question}>
+                          <Form.Item
+                            name={currentQuestion.id}
+                            rules={[
+                              {
+                                required: true,
+                                message: 'This question cannot be left empty',
+                              },
+                            ]}
+                          >
+                            <TextArea rows={15} />
+                          </Form.Item>
+                        </Card>
+                      )}
+                    </Col>
+                  </Row>
+                </Card>
+              )}
+            </Form>
+          </div>
         </Spin>
         <div className={styles.push} />
       </div>
