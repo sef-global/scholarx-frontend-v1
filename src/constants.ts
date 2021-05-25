d6cd0ece0013ab0b133b6f8cd9cf1eb54ff61ff2
kumuditha-udayanga <kumudithaudaya@gmail.com>
2021-05-25 23:42:02 +0530

    Update the mentee apply page content

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const API_URL = 'http://localhost:8080/api';
 export const AUTH_URL = 'http://localhost:8080/login';
 export const LOGOUT_URL = 'http://localhost:8080/logout';
+export const APPLICATION_TEMPLATE =
+  'https://docs.google.com/document/d/1pRd2Mml7ejjY9rbgQS0PEo3AWooenHWKsXWBAQmnw9Q/edit?usp=sharing';
