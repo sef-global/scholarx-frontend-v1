cd6b34194a8712fb63a2bdd0f3d3467e7419182a
anjula-sack <anjulashanaka@gmail.com>
2021-11-27 15:01:59 +0530

    Add the manage mentees table

--- src/scenes/Dashboard/scenes/ManageMentees/index.tsx
+++ src/scenes/Dashboard/scenes/ManageMentees/index.tsx
@@ -1,25 +1,56 @@
 import React, { useEffect, useState } from 'react';
 
-import { WarningOutlined } from '@ant-design/icons';
-import {
-  Typography,
-  List,
-  Avatar,
-  notification,
-  Spin,
-  Button,
-  Modal,
-} from 'antd';
+import { Typography, notification, Spin, Button, Select, Table } from 'antd';
 import axios, { AxiosResponse } from 'axios';
 import { useParams } from 'react-router';
 
 import { API_URL } from '../../../../constants';
 import { Mentee } from '../../../../types';
 import styles from '../../styles.css';
-import StatusTag from '../ManageMentors/components/StatusTag';
 
 const { Title } = Typography;
-const { confirm } = Modal;
+const { Option } = Select;
+
+const columns = [
+  {
+    title: 'Mentee',
+    dataIndex: 'name',
+    filters: [],
+  },
+  {
+    title: 'Primary Mentor',
+    dataIndex: 'primaryMentor',
+  },
+  {
+    title: 'Assigned To',
+    filters: [],
+    render: () => (
+      <Select showSearch style={{ width: 200 }} placeholder="Select a person">
+        <Option value="jack">Jack</Option>
+        <Option value="lucy">Lucy</Option>
+        <Option value="tom">Tom</Option>
+      </Select>
+    ),
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    filters: [],
+    render: () => (
+      <Select showSearch style={{ width: 200 }} placeholder="Select a state">
+        <Option value="PENDING">PENDING</Option>
+        <Option value="ASSIGNED">ASSIGNED</Option>
+        <Option value="POOL">POOL</Option>
+        <Option value="DISCARDED">DISCARDED</Option>
+      </Select>
+    ),
+  },
+  {
+    title: '',
+    dataIndex: 'status',
+    render: () => <Button type="link">View Application</Button>,
+  },
+];
 
 function ManageMentees() {
   const { programId } = useParams();
@@ -49,81 +80,11 @@ function ManageMentees() {
       });
   }, []);
 
-  const removeMentee = (id: number) => {
-    confirm({
-      title: 'Do you want to remove this mentee?',
-      icon: <WarningOutlined />,
-      content: 'This action is not reversible. Please confirm below.',
-      onOk() {
-        axios
-          .delete(`${API_URL}/admin/mentees/${id}`, {
-            withCredentials: true,
-          })
-          .then((result: AxiosResponse) => {
-            if (result.status == 204) {
-              notification.success({
-                message: 'Success!',
-                description: 'Successfully removed the mentee',
-              });
-            } else {
-              throw new Error();
-            }
-          })
-          .catch(() => {
-            notification.error({
-              message: 'Error!',
-              description: 'Something went wrong when deleting the mentee',
-            });
-          });
-      },
-    });
-  };
-
   return (
     <div className={styles.container}>
       <Title>Manage Mentees</Title>
       <Spin tip="Loading..." spinning={isLoading}>
-        <List
-          itemLayout="horizontal"
-          size="large"
-          pagination={{
-            pageSize: 8,
-          }}
-          dataSource={mentees}
-          renderItem={(item: Mentee) => (
-            <List.Item
-              key={item.id}
-              actions={[
-                <Button
-                  key="remove"
-                  type="primary"
-                  onClick={() => removeMentee(item.id)}
-                  danger
-                >
-                  Remove
-                </Button>,
-              ]}
-            >
-              <List.Item.Meta
-                avatar={<Avatar src={item.profile.imageUrl} />}
-                title={
-                  <a href={item.profile.linkedinUrl}>
-                    {item.profile.firstName} {item.profile.lastName}
-                  </a>
-                }
-                description={item.profile.headline}
-              />
-              <span className={styles.mentorNameSpan}>
-                {item.profile.email}
-              </span>
-              <span className={styles.mentorNameSpan}>
-                Mentor: {item.mentor.profile.firstName}{' '}
-                {item.mentor.profile.lastName}
-              </span>
-              <StatusTag state={item.state} />
-            </List.Item>
-          )}
-        />
+        <Table columns={columns} dataSource={mentees} />
       </Spin>
     </div>
   );
