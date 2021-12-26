17d651591d3f1ccf8e1f8970469df1db84e76f7c
piumal1999 <piumalrathnayake@hotmail.com>
2021-12-27 00:56:01 +0530

    Display mentor details during Mentee application period

--- src/scenes/Home/scenes/RequestMentors/scenes/RequestedMentor/index.tsx
+++ src/scenes/Home/scenes/RequestMentors/scenes/RequestedMentor/index.tsx
@@ -8,16 +8,13 @@ import {
   Spin,
   Row,
   Col,
-  Form,
   Tabs,
-  Button,
 } from 'antd';
-import axios, { AxiosResponse, Method } from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { useHistory, useParams } from 'react-router';
 
-import MentorResponses from '../../../../../../components/MentorResponses';
 import { API_URL } from '../../../../../../constants';
-import { Mentee, Mentor } from '../../../../../../types';
+import { Mentor } from '../../../../../../types';
 import styles from './styles.css';
 
 const { Title, Text } = Typography;
@@ -29,9 +26,7 @@ function RequestedMentor() {
     programId: string,
   }>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isApplied, setIsApplied] = useState<boolean>(false);
   const [mentor, setMentor] = useState<Mentor>(null);
-  const [form] = Form.useForm();
   const history = useHistory();
 
   useEffect(() => {
@@ -55,89 +50,14 @@ function RequestedMentor() {
           description: 'Something went wrong when fetching the mentor',
         });
       });
-
-    axios
-      .get(`${API_URL}/mentors/${mentorId}/mentee`, {
-        withCredentials: true,
-      })
-      .then((result: AxiosResponse<Mentee>) => {
-        if (result.status == 200) {
-          form.setFieldsValue({ submissionURL: result.data.submissionUrl });
-          setIsLoading(false);
-          setIsApplied(true);
-        } else if (result.status == 204) {
-          setIsApplied(false);
-        } else {
-          throw new Error();
-        }
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        notification.error({
-          message: error.toString(),
-          description: 'Something went wrong when fetching the mentee',
-        });
-      });
   }, []);
-
-  const requestMentor = (values: any) => {
-    const submissionUrl: string = values.submissionURL;
-    let statusCode: number, method: Method;
-    if (isApplied) {
-      statusCode = 200;
-      method = 'put';
-    } else {
-      statusCode = 201;
-      method = 'post';
-    }
-    axios({
-      method: method,
-      url: `${API_URL}/mentors/${mentorId}/mentee`,
-      data: { submissionUrl: submissionUrl },
-      withCredentials: true,
-    })
-      .then((res: AxiosResponse<Mentee>) => {
-        if (res.status == statusCode) {
-          setIsLoading(false);
-          if (statusCode == 201) {
-            setIsApplied(true);
-          }
-          notification.success({
-            message: 'Success!',
-            description: 'Successfully applied!',
-          });
-        } else {
-          throw new Error();
-        }
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        notification.error({
-          message: error,
-          description: 'Something went wrong when requesting the mentor',
-        });
-      });
-  };
 
   const onBack = () => {
     history.push(`/program/${programId}`);
   };
 
-  const onApply = () => {
-    history.push(`/program/${programId}/mentor/4/apply`); // Hardcoded mentorId
-  };
-
   return (
     <>
-      <Button
-        type="primary"
-        size="large"
-        onClick={onApply}
-        className={styles.button}
-      >
-        Apply
-      </Button>
-      <br />
       <Tabs defaultActiveKey="1" onTabClick={onBack}>
         <TabPane tab="Mentors" key="1" />
       </Tabs>
@@ -152,7 +72,11 @@ function RequestedMentor() {
                 {mentor?.profile.firstName} {mentor?.profile.lastName}
               </Title>
               <Text>{mentor?.profile.headline}</Text>
-              <a href={mentor?.profile.linkedinUrl} target="_blank">
+              <a
+                href={mentor?.profile.linkedinUrl}
+                rel="noreferrer"
+                target="_blank"
+              >
                 <LinkedinOutlined />
                 {''} {mentor?.profile.firstName}&apos;s LinkedIn profile
               </a>
@@ -161,20 +85,36 @@ function RequestedMentor() {
           <br />
           <div className={styles.contentMargin}>
             <Col span={12}>
-              <MentorResponses
-                programId={Number(programId)}
-                mentorId={Number(mentorId)}
-              />
+              <Row>
+                <Text strong>Category</Text>
+              </Row>
+              <Row>{mentor?.category.replace('_', ' ')}</Row>
+              <br />
+              <Row>
+                <Text strong>Expertise</Text>
+              </Row>
+              <Row>{mentor?.expertise}</Row>
+              <br />
+              <Row>
+                <Text strong>Institution</Text>
+              </Row>
+              <Row>{mentor?.institution}</Row>
+              <br />
+              <Row>
+                <Text strong>Position</Text>
+              </Row>
+              <Row>{mentor?.position}</Row>
+              <br />
+              <Row>
+                <Text strong>Bio</Text>
+              </Row>
+              <Row>{mentor?.bio}</Row>
+              <br />
+              <Row>
+                <Text strong>Available Mentee Slots</Text>
+              </Row>
+              <Row>{mentor?.slots}</Row>
             </Col>
-            <h2>Pre-requisites:</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur.
-            </p>
           </div>
           <hr className={styles.horizontalLine} />
         </Spin>
