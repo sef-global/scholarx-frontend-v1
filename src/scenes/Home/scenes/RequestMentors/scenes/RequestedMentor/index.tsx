dc09164a62b1a1d2aeb23c89666ed02dd898ace6
piumal1999 <piumalrathnayake@hotmail.com>
2021-12-29 23:12:38 +0530

    Replace mentor details with mentor card

--- src/scenes/Home/scenes/RequestMentors/scenes/RequestedMentor/index.tsx
+++ src/scenes/Home/scenes/RequestMentors/scenes/RequestedMentor/index.tsx
@@ -1,23 +1,14 @@
 import React, { useEffect, useState } from 'react';
 
-import { LinkedinOutlined } from '@ant-design/icons';
-import {
-  Typography,
-  Avatar,
-  notification,
-  Spin,
-  Row,
-  Col,
-  Tabs,
-} from 'antd';
+import { notification, Spin, Col, Tabs } from 'antd';
 import axios, { AxiosResponse } from 'axios';
 import { useHistory, useParams } from 'react-router';
 
+import MentorProfileCard from '../../../../../../components/MentorProfileCard';
 import { API_URL } from '../../../../../../constants';
 import { Mentor } from '../../../../../../types';
 import styles from './styles.css';
 
-const { Title, Text } = Typography;
 const { TabPane } = Tabs;
 
 function RequestedMentor() {
@@ -63,57 +54,9 @@ function RequestedMentor() {
       </Tabs>
       <div className={styles.textPadding}>
         <Spin tip="Loading..." spinning={isLoading}>
-          <Row>
-            <Col offset={1} span={1}>
-              <Avatar size={64} src={mentor?.profile.imageUrl} />
-            </Col>
-            <Col offset={1}>
-              <Title level={3}>
-                {mentor?.profile.firstName} {mentor?.profile.lastName}
-              </Title>
-              <Text>{mentor?.profile.headline}</Text>
-              <a
-                href={mentor?.profile.linkedinUrl}
-                rel="noreferrer"
-                target="_blank"
-              >
-                <LinkedinOutlined />
-                {''} {mentor?.profile.firstName}&apos;s LinkedIn profile
-              </a>
-            </Col>
-          </Row>
-          <br />
           <div className={styles.contentMargin}>
             <Col span={12}>
-              <Row>
-                <Text strong>Category</Text>
-              </Row>
-              <Row>{mentor?.category.replace('_', ' ')}</Row>
-              <br />
-              <Row>
-                <Text strong>Expertise</Text>
-              </Row>
-              <Row>{mentor?.expertise}</Row>
-              <br />
-              <Row>
-                <Text strong>Institution</Text>
-              </Row>
-              <Row>{mentor?.institution}</Row>
-              <br />
-              <Row>
-                <Text strong>Position</Text>
-              </Row>
-              <Row>{mentor?.position}</Row>
-              <br />
-              <Row>
-                <Text strong>Bio</Text>
-              </Row>
-              <Row>{mentor?.bio}</Row>
-              <br />
-              <Row>
-                <Text strong>Available Mentee Slots</Text>
-              </Row>
-              <Row>{mentor?.slots}</Row>
+              {mentor && <MentorProfileCard mentor={mentor} />}
             </Col>
           </div>
           <hr className={styles.horizontalLine} />
