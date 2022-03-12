dfb073b9ba6e1140477de8b6dd67df3c071dedfa
Anjula Shanaka <anjulashanaka@gmail.com>
2022-03-12 17:52:34 +0530

    Integrate hotjar (#318)

--- src/util/google-analytics.ts
+++
@@ -1,8 +0,0 @@
-import ReactGA from 'react-ga';
-
-/**
- * Tracks visited page path with Google Analytics.
- */
-export const trackPageWithGoogleAnalytics = () => {
-  ReactGA.pageview(window.location.pathname + window.location.search);
-};
